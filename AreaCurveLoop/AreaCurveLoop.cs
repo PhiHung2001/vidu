1a250f4a97d7abf395883a2bc8d167a1d27a0488
Phi Hung <vophihung10052001@gmail.com>
2023-09-04 15:55:56 +0700

    ok

---
+++ AreaCurveLoop/AreaCurveLoop.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCurveLoop
+{
+    public class AreaCurveLoop
+    {
+        public double AreaFromListCurveLoop(IList<CurveLoop> curveLoopList)
+        {
+            double area = 0.0;
+            SolidOptions options = new SolidOptions(ElementId.InvalidElementId, ElementId.InvalidElementId);
+
+            Solid solid = GeometryCreationUtilities.CreateExtrusionGeometry(curveLoopList, XYZ.BasisZ, 1.0, options);
+
+            Face face = solid.Faces.get_Item(0) as Face;
+
+            return face.Area/10.763;
+        }
+        
+    }
+
+}
---
+++ AreaCurveLoop/Class1.cs
@@ -0,0 +1,32 @@
+﻿
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application = Autodesk.Revit.ApplicationServices.Application;
+
+namespace AreaCurveLoop
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+
+            
+
+            return Result.Succeeded;
+        }
+    }
+}
